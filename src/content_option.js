68b26b177fd1f102dc3826de196483c2f88a256f
ManishKumar180 <manishkumar97430@gmail.com>
2022-07-28 18:17:53 +0530

    First commit

---
+++ src/content_option.js
@@ -0,0 +1,157 @@
+const logotext = "Manish";
+const meta = {
+    title: "Manish Kumar",
+    description: "I’m a Front End Developer _ and learning everyday to sharp my skills and be a good Developer ",
+};
+
+const introdata = {
+    title: "I’m Manish Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I like to learn about Tech",
+    },
+    description: "Hey, I am a passionate Front-End-Developer , self taught dev and stack overflow addict lol  ",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my Self",
+    aboutme: "Hello, I'm a Front End Developer by self training and Full Stack Developer by passion who loves learning  and building for the web :)",
+};
+const worktimeline = [{
+        jobtitle: "Front End Trainee",
+        where: "Prutor.ai",
+        date: "2021",
+    },
+    {
+        jobtitle: "Front End Developer",
+        where: "Spark Foundation",
+        date: "2022",
+    },
+    
+];
+
+const skills = [{
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "Express",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Node Js",
+        value: 65,
+
+    },
+
+    {
+        name : "Figma",
+        value : 60,
+    },
+    {
+        name: "Spline ",
+        value : 50
+    },
+    {
+        name : "Wordpress",
+        value : 68
+    },
+    {
+        name : "Bootstrap",
+        value : 75,
+    },
+    {
+        name : "Material UI",
+        value : 70
+    },{
+        name : "Git ",
+        value : 65
+    },
+    {
+        name: "Linux",
+        value : 65
+    }
+];
+
+const services = [{
+        title: "Protototyping",
+        
+    },
+    {
+        title: "Front End Development",
+        
+    },
+    {
+        title: "Wordpress Design",
+        description: "",
+    },
+    {
+        title: "Seach Engine optimization (S.E.O)"
+    }
+];
+
+const dataportfolio = [{
+        img: "https://mkdev.vercel.app/_next/image?url=%2Fstatic%2Fimages%2Fprojects%2Fcovid.png&w=640&q=75/?grayscale",
+        desctiption: "Web App to visualize sorting Algorithms made using ReactJs",
+        link: "https://github.com/ManishKumar180/Dsa_Sorting_Visualizer",
+    },
+    {
+        img: "https://i.ytimg.com/vi/cNmn72kiZWU/maxresdefault.jpg",
+        desctiption: "Pokemon Pokedex made using Rest Apis ",
+        link: "https://github.com/ManishKumar180/pokemon_deX",
+    },
+    {
+        img: "https://camo.githubusercontent.com/758789925c77a28be1006b2684038407dafa31eb22fb8dfbc615f355d760cf20/68747470733a2f2f692e6962622e636f2f583837427156592f53637265656e73686f742d323032302d30342d31332d61742d31302d31342d35382e706e67" ,
+        desctiption: "Covid-19 Tracker  made using Rest Api , React Js, ChartJs  ",
+        link: "https://github.com/ManishKumar180/cov19_tracker",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "Personal Blog made using NextJs and Tailwind  ",
+        link: "https://github.com/ManishKumar180/mkdev",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "manishkumar97430@gmail.com",
+    YOUR_FONE: "91-630-7046-741",
+    description: " ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ManishKumar180/ManishKumar180",
+    linkedin: "https://www.linkedin.com/in/manish-kumar-6309b0214",
+    twitter: "https://twitter.com/@code4jobbruh",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
